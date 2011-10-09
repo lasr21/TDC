e0f8f7a1d06a68b1f307acd6675f3a90c1be8fee
LASR21 <lasr21@TESLA.(none)>
2011-10-09 02:59:07 -0500

    ya pude modificar la imagen

--- tdc.py
+++ tdc.py
@@ -1,9 +1,14 @@
 import Image
 import os
+import StringIO
 
 im = Image.open("elena.jpg").convert("L")
 
 
 miau = list(im.getdata())
+print (im.getpixel((2,2)))
 
-print(miau)
+for n in  range(0,256):
+	for m in range(0,256):
+		im.putpixel((n,m),200)
+im.show()
